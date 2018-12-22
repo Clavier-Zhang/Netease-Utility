d11cca30451bfb4d9ad781c2177aebcdf07089f5
Clavier <y2595zhang@edu.wwaterloo.ca>
2018-12-22 14:44:43 -0500

    233

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import requests
+import _thread
+import threading
+import time
+import pymongo
+
 
 url = 'http://localhost:3000'
 phone = '13962125149'
 password = 'zyc990610'
+proxy_server = 'http://localhost:8080'
 
+
+db = pymongo.MongoClient('www.clavier.moe', 27017).net_ease.user
+
+class ProxyPool:
+    count = 0
+    current = {'https':''}
+    api = ""
+    def __init__(self, proxy_server):
+        self.api = proxy_server + '/get'
+        response = requests.get(self.api)
+        self.current['https'] = response.text
+        self.count = 0
+
+    def update(self):
+        response = requests.get(self.api)
+        self.current['https'] = response.text
+        self.count = 0
+
+    def get(self):
+        self.count = self.count + 1
+        if self.count == 20:
+            self.update()
+        return self.current
 
 # login
 def login(phone, password):
@@ -21,34 +50,52 @@ def get_user_detail(uid):
     print(response.json())
     print('get user detail successfully')
 
+
+
 def user_exist(uid, proxies):
     api = '/user/detail'
-    params = {'uid':uid}
+    params = {'uid':str(uid)}
     response = requests.get(url + api, params=params, cookies=cookies, proxies=proxies)
     json = response.json()
     if json['code'] == 404:
         print('user not exist')
         return False
-    
+    db.insert_one({'uid':uid})
     print('user exists')
     return True
 
-def get_proxy():
-    api = 'http://localhost:8080/get'
-    response = requests.get(api)
-    proxy = response.text
-    return proxy
 
-
+pool = ProxyPool(proxy_server)
 cookies = login(phone, password)
-proxies = {'https':'http://' + get_proxy()}
-count = 0
 
-user_exist('32953014', proxies)
+def test(low, high):
+    for i in range(low, high):
+        print(time.ctime(time.time()))
+        user_exist(i, pool.get())
 
-for i in range(32953014, 32954114):
-    user_exist(str(i), proxies)
-    count = count + 1
-    if count == 20:
-        count = 0
-        proxies['https'] = 'http://' + get_proxy()
+
+# thread1 = threading.Thread(target=test, args=(32953014,33953014))
+# thread2 = threading.Thread(target=test, args=(31953014,32953014))
+# thread3 = threading.Thread(target=test, args=(30953014,31953014))
+# thread4 = threading.Thread(target=test, args=(33953014,34953014))
+# thread5 = threading.Thread(target=test, args=(32953014,33953014))
+# thread6 = threading.Thread(target=test, args=(31953014,32953014))
+# thread7 = threading.Thread(target=test, args=(30953014,31953014))
+# thread8 = threading.Thread(target=test, args=(33953014,34953014))
+
+# thread1.start()
+# thread2.start()
+# thread3.start()
+# thread4.start()
+# thread5.start()
+# thread6.start()
+# thread7.start()
+# thread8.start()
+
+# db.close()
+
+
+# random
+# print(list(db.aggregate([{ '$sample': { 'size': 1 } }])))
+
+# print(db.aggregate( {$sample: {size:1}} )
