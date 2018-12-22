ecd7a04f8292a528c5f99ab143c10336d96fccb6
Clavier <y2595zhang@edu.wwaterloo.ca>
2018-12-22 15:41:24 -0500

    233

--- main.py
+++ main.py
@@ -3,34 +3,15 @@ import _thread
 import threading
 import time
 import pymongo
-
+from proxy_pool import ProxyPool
 
 url = 'http://localhost:3000'
-phone = '13962125149'
-password = 'zyc990610'
+phone = '17005769034'
+password = 'aaaa8888'
 proxy_server = 'http://localhost:8080'
 db_server = 'www.clavier.moe'
 
-class ProxyPool:
-    count = 0
-    current = {'https':''}
-    api = ""
-    def __init__(self, proxy_server):
-        self.api = proxy_server + '/get'
-        response = requests.get(self.api)
-        self.current['https'] = response.text
-        self.count = 0
 
-    def update(self):
-        response = requests.get(self.api)
-        self.current['https'] = response.text
-        self.count = 0
-
-    def get(self):
-        self.count = self.count + 1
-        if self.count == 20:
-            self.update()
-        return self.current
 
 class Database:
     current = 0
@@ -40,7 +21,6 @@ class Database:
         lst = list(self.db.find().sort('uid', -1).limit(1))
         if len(lst) != 0:
             self.current = lst[0]['uid']
-        self.db.delete_many({})
     
     def save(self,uid):
         self.db.insert_one({'uid':uid})
@@ -48,6 +28,10 @@ class Database:
     def get(self):
         self.current = self.current + 1
         return self.current
+
+    def clean(self):
+        self.db.delete_many({})
+
 
 
 # login
@@ -58,6 +42,10 @@ def login(phone, password):
     cookies = response.cookies
     print('login successfully')
     return cookies
+
+pool = ProxyPool(proxy_server)
+cookies = login(phone, password)
+db = Database(db_server)
 
 def get_user_detail(uid):
     api = '/user/detail'
@@ -70,30 +58,31 @@ def get_user_detail(uid):
 
 def user_exist(uid, proxies,db):
     api = '/user/detail'
-    params = {'uid':str(uid)}
+    # params = {'uid':str(uid)}
+    params = {'uid':str(32953014)}
     response = requests.get(url + api, params=params, cookies=cookies, proxies=proxies)
     json = response.json()
-    if json['code'] == 404:
-        print('user not exist')
+    print(json)
+    if json['code'] != 200:
+        print('error')
+        print(json)
         return False
     db.save(uid)
     print('user' + str(uid) + 'exists')
     return True
 
 
-pool = ProxyPool(proxy_server)
-cookies = login(phone, password)
-db = Database(db_server)
+
 
 def test():
     while 1:
         print(time.ctime(time.time()))
         user_exist(db.get(),pool.get(),db)
 
+db.clean()
+thread1 = threading.Thread(target=test)
 
-# thread1 = threading.Thread(target=test)
-
-# thread1.start()
+thread1.start()
 
 # db.close()
 
---
+++ proxy_pool.py
@@ -0,0 +1,22 @@
+import requests
+
+class ProxyPool:
+    count = 0
+    current = {'https':''}
+    api = ""
+    def __init__(self, proxy_server):
+        self.api = proxy_server + '/get'
+        response = requests.get(self.api)
+        self.current['https'] = response.text
+        self.count = 0
+
+    def update(self):
+        response = requests.get(self.api)
+        self.current['https'] = response.text
+        self.count = 0
+
+    def get(self):
+        self.count = self.count + 1
+        if self.count == 20:
+            self.update()
+        return self.current
